209016d2fc621d56c7bcc77419a1b072ac19781c
Tobias Brockhoff <brockhoff@pads.rwth-aachen.de>
2025-03-14 23:29:57 +0100

    Forgot a few imports when copying.

--- src/org/processmining/trafficlightcc/algorithms/PartialRunInstanceCreator.java
+++ src/org/processmining/trafficlightcc/algorithms/PartialRunInstanceCreator.java
@@ -15,6 +15,8 @@ import org.deckfour.xes.classification.XEventAttributeClassifier;
 import org.deckfour.xes.classification.XEventClass;
 import org.deckfour.xes.classification.XEventClasses;
 import org.deckfour.xes.classification.XEventClassifier;
+import org.deckfour.xes.info.XLogInfo;
+import org.deckfour.xes.info.XLogInfoFactory;
 import org.deckfour.xes.model.XEvent;
 import org.deckfour.xes.model.XLog;
 import org.deckfour.xes.model.XTrace;
